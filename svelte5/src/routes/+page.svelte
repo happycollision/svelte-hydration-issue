2d04103eb0d32248b8e6e9af9b546b26dfa1efc2
Don Denton <don@happycollision.com>
2024-07-07 16:55:16 -0400

    initial

---
+++ svelte5/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { browser } from "$app/environment"
+	const renderLocation = browser ? "In browser" : "On server"
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+Standard: {renderLocation}
+<br />
+@html: {@html renderLocation}
